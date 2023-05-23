3093e2a91ed142a4dc4b7fe7bd1b1525aba3512c
imtanay <107740662+imTanay0@users.noreply.github.com>
2023-05-23 21:15:04 +0530

    Add List of available users in users page

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   images: {
     domains: [
       "res.cloudinary.com",
-      "avatars.gihubusercontent.com",
+      "avatars.githubusercontent.com",
       "lh3.googleusercontent.com"
     ]
   }
